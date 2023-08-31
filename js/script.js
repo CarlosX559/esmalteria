fbca4b4c42c10477b81d2f56f1c371b5ae65dae8
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-08-30 22:09:19 -0300

    add responsive 375px->1440px

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function menu() {
     $(".menu_mobile_open").click(
         function () {
 
-            $('.menu_mobile').css('display', 'flex').css('top', '95px').css('animation', 'move ease-in 400ms');
+            $('.menu_mobile').css('display', 'flex').css('top', '150px').css('animation', 'move ease-in 400ms');
             setTimeout(() => {
                 $('.menu_mobile_open').css('display', 'none');
                 $('.menu_mobile_close').css('display', 'flex');
@@ -29,3 +29,35 @@ function menu() {
     );
 }
 menu();
+
+
+function modal() {
+
+    let service = document.querySelectorAll('.area_principal_servicos');
+
+    service.forEach((element) => {
+
+        element.addEventListener('click', () => {
+            document.querySelector('.area_modal').style.display = 'flex';
+
+            let title = document.querySelector('.title_modal h2');
+            let sub_title = document.querySelector('.sub_title_modal p');
+            let image = document.querySelector('.modal_img');
+
+            title.innerHTML = element.getAttribute('data-title');
+            sub_title.innerHTML = element.getAttribute('data-sub-title');
+            image.src = element.getAttribute('data-src');
+
+        });
+
+    });
+
+    let close_modal = document.querySelector('.close_modal');
+
+    close_modal.addEventListener('click', () => {
+        document.querySelector('.area_modal').style.display = 'none';
+    })
+
+}
+
+modal();
